5c999a29e54b85d086a70c0c693c1bb473450398
marc thomas <marc.thomas5608@gmail.com>
2026-04-01 09:35:40 +0200

    changement du schema

--- db/schema.rb
+++ db/schema.rb
@@ -212,9 +212,22 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_170749) do
     t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
   end
 
-# Could not dump table "toys" because of following StandardError
-#   Unknown type 'vector(1536)' for column 'embedding'
-
+  create_table "toys", force: :cascade do |t|
+    t.decimal "price"
+    t.string "barcode"
+    t.boolean "complete"
+    t.boolean "clean"
+    t.boolean "playable"
+    t.bigint "box_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location", default: "En attente de validation"
+    t.string "status", default: "pending", null: false
+    t.vector "embedding", limit: 1536
+    t.index ["box_id"], name: "index_toys_on_box_id"
+    t.index ["category_id"], name: "index_toys_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false, null: false
